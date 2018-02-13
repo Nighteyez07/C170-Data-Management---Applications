870d9ab3604c604d64e59f1321e9e50a97fadfd7
Nighteyez07 <Nighteyez07@gmail.com>
2018-02-12 18:53:05 -0800

    Refactor to remove Customer table in second normal form and add Line Item table. Updated explanations to cover new table. Used Lucidchart for entity and ER diagrams

---
+++ C170/Line_Item_2NF.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Line_Item_2NF]
+(
+	[ProductId] INT NOT NULL, 
+	[SaleId] INT NOT NULL, 
+	[Quantity] NCHAR(10) NOT NULL, 
+	CONSTRAINT [FK_Line_Item_2NF_Sale] FOREIGN KEY ([SaleId]) REFERENCES [Sales_2NF]([SaleId]),
+	CONSTRAINT [FK_Line_Item_2NF_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product_2NF]([ProductId]), 
+	CONSTRAINT [PK_Line_Item_2NF] PRIMARY KEY ([SaleId], [ProductId])
+)
--- C170/Sales_2NF.sql
+++ C170/Sales_2NF.sql
@@ -6,7 +6,15 @@
 	[ProductId] INT NOT NULL, 
 	[Quantity] INT NOT NULL, 
 	[SpecialHandlingNotes] NVARCHAR(500) NULL,
+	[LastName] NVARCHAR(50) NOT NULL, 
+	[FirstName] NVARCHAR(50) NOT NULL, 
+	[Address1] NVARCHAR(250) NOT NULL, 
+	[Address2] NVARCHAR(250) NULL, 
+	[City] NVARCHAR(50) NOT NULL, 
+	[State] NCHAR(2) NOT NULL, 
+	[Zip] NCHAR(6) NOT NULL,
+	[CustomerHomePhone] NCHAR(10) NULL, 
+	[CustomerMobilePhone] NCHAR(10) NULL, 
+	[CustomerOtherPhone] NCHAR(10) NULL,
 	CONSTRAINT [PK_Sales_2NF] PRIMARY KEY ([SaleId]),
-	CONSTRAINT [FK_Sales_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer_2NF]([CustomerId]), 
-	CONSTRAINT [FK_Sales_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product_2NF]([ProductId])
 )
